fd4f8ee3b5d1630b066d208b3bfbb0c4034ad73d
WilmarAgudelo <wagudelo352@gmail.com>
2024-05-26 11:45:59 -0500

    funciona

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
-export default defineConfig({
-  plugins: [vue()],
-  base: 'https://WilmarAgudelo.github.io/proyecto',
-});
-
-
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
